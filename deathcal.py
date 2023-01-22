48a8959654a076502ca9471c993981a0de55d7a6
Luciano Kumaru <78513827+frax27@users.noreply.github.com>
2023-01-22 01:45:03 -0500

    added years to the calculation

--- deathcal.py
+++ deathcal.py
@@ -4,5 +4,6 @@ years_remaining = 90 - age_as_int
 days_remaining = years_remaining * 365
 weeks_remaining = years_remaining * 52
 months_remaining = years_remaining * 12
-message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left"
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months, and {years_remaining} years32 left"
 print(message)
+
